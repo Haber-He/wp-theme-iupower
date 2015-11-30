d9bf7af85a28bf6809e79134e5e35d57a4493226
Haber He <Haber.He@outlook.com>
2015-11-30 23:08:10 +0800

    初始化

---
+++ category.php
@@ -0,0 +1,40 @@
+
+<?php get_header(); ?>
+
+<?php get_sidebar('top'); ?>
+
+<div class="inner container"> 
+    <div class="column-fluid"> 
+        <div class="content">
+        <?php
+            if(is_category()) :
+            
+            $cat = get_query_var('cat');
+            $yourcat = get_category($cat);
+            $cat_id = $yourcat->term_id;
+                
+            $limit = get_option('posts_per_page');
+            
+            if ( !empty($cat_id) ):
+                $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+                $args = array(
+                    'cat'       => $cat_id,
+                    'paged'     => $paged,
+                    'orderby'   => 'date',
+                    'order'     => 'DESC'
+                );
+                $posts = query_posts($args);
+        ?>
+        
+            <?php get_template_part('content', get_cat_templage($cat_id)); ?>
+            
+        <?php endif; wp_reset_postdata();?>
+        <?php endif; ?>
+        </div> 
+    </div>
+
+    <?php get_sidebar('left'); ?>
+    <div class="clearfix"></div> 
+</div>
+
+<?php get_footer(); ?>
